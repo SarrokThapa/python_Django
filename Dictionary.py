0e0567ae676d75fe6c5d2f146ea3a43cd464e633
SarrokThapa <sarrok.thapa.s24@icp.edu.np>
2025-11-06 21:26:23 +0545

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+my_details = {"Name":"Sarrok Thapa","Age":20,"PhoneNo.":9806502018,"Email":"sarrokthapa18@gmail.com",
+#List
+"hobbies" : ["Playing Games","Running","Baking","Cooking"],
+#tuple
+"DOB" : (2005,"April",8),
+#Set 
+"fav_food" : ("Momo","Burger","Cookies")}
+
+print (my_details["hobbies"].append("Flying"))
+print(my_details)
